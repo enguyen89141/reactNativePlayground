22b56e81c0267a690df86ab2f19585cbc4faa449
Eric Nguyen <enguyen89141@gmail.com>
2020-03-11 15:46:19 -0700

    added flatlist componenent need to add key

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,13 +1,31 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+
 
 const ListScreen = () => {
-    return <Text>List Screen</Text>;
+    const friends = [
+        { name: 'Friend 1' },
+        { name: 'Friend 10' },
+        { name: 'Friend 100' },
+        { name: 'Friend 1000' },
+        { name: 'Friend 10000' },
+        { name: 'Friend 100000' },
+        { name: 'Friend 1000000' },
+    ];
+
+    return (
+        <FlatList
+            data={friends}
+            renderItem={({ item }) => {
+                return <Text>{item.name}</Text>
+            }}
+        />
+    )
 }
 
 const styles = StyleSheet.create({
 
-    
+
 });
 
 export default ListScreen;
